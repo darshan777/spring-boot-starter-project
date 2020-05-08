29a68738004308ee2a5cc0ecad6e7255f7354494
Darshan <sanghavi.da@husky.neu.edu>
2020-05-08 19:01:04 -0400

    crud operations for user

--- spring-boot-sample-project/src/main/java/com/example/demo/service/UserService.java
+++ spring-boot-sample-project/src/main/java/com/example/demo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.demo.service;
 
 import com.example.demo.pojo.User;
+import com.example.demo.repository.UserRRepository;
 import com.example.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,40 +10,28 @@ import java.util.*;
 
 @Component
 public class UserService {
-
-
+    @Autowired
+    private UserRRepository userRRepository;
     private UserRepository userRepository = new UserRepository();
 
     public List<User> getAllUser(){
-        List<User> userList = userRepository.getUserList();
+        List<User> userList = userRRepository.findAll();
         return userList;
     }
 
-    public User getAUser(String name){
-        List<User> userList = userRepository.getUserList();
-        for(User u : userList){
-            if(u.getFirstName().equalsIgnoreCase(name)){
-                return u;
-            }
-        }
-        return null;
+    public Optional<User> getAUser(int id){
+        Optional<User> u = userRRepository.findById(id);
+
+        return u;
     }
 
     public boolean createUser(User user){
-        userRepository.getUserList().add(user);
+        userRRepository.save(user);
         return true;
     }
 
-    public User updateUser(String userName, User user){
-        List<User> userList = userRepository.getUserList();
-        for(User u : userList){
-            if(u.getFirstName().equalsIgnoreCase(userName)){
-                userRepository.getUserList().remove(u);
-                userRepository.getUserList().add(user);
-                return user;
-            }
-        }
-        return null;
+    public boolean updateUser(int userId, User user){
+        return true;
     }
 
     public boolean deleteUser(String userName){
